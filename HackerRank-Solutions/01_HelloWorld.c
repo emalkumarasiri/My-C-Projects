24ffc5196e985d27f2a39f790c7d3d3d6f00f311
Emal Kumarasiri <emalkumarasiri2006@gmail.com>
2026-07-17 14:09:01 +0530

    Update 01_HelloWorld.c

--- HackerRank-Solutions/01_HelloWorld.c
+++ HackerRank-Solutions/01_HelloWorld.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
 int main() 
 {
-	
-    char s[50];
-    fgets(s,100,stdin);
-    printf("Hello, World!\n%s ",s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+	printf("Hello, World!");	 
     return 0;
 }
