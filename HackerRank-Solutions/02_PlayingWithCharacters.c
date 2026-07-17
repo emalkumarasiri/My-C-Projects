66ec3d7e635b1ffa839831111c01089a168f37be
Emal Kumarasiri <emalkumarasiri2006@gmail.com>
2026-07-17 14:10:04 +0530

    Create 02_PlayingWithCharacters.c

---
+++ HackerRank-Solutions/02_PlayingWithCharacters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[50];
+    fgets(s,100,stdin);
+    printf("Hello, World!\n%s ",s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
